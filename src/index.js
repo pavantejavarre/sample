9fad35037470049d4d3433e03d94f0ff0ce86f96
pavantejavarre <pavnateja0891@gmail.com>
2022-05-19 14:30:30 -0400

    function comp

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDom from 'react-dom';
 import App from './App';
 
-let one = <H1> CHANGE</H1>
-ReactDom.render(one, document.getElementById("root"))
+
+ReactDom.render(<App/>, document.getElementById("root"))
