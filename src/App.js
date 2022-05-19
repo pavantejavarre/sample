06c74c3cdec3f2c473d60282fa370b45c2101fdb
pavantejavarre <pavnateja0891@gmail.com>
2022-05-19 14:23:31 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App()
+{
+  return(
+    <p> i have created a function components with out using props and wantd to push it to git</p>
+  )
+}
+
+export default App;
